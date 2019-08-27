c2a210f4d98777045987861f6bbf6c7a22dca39b
Charles Torre <torre_charles@hotmail.com>
2019-08-27 13:23:53 -0700

    Update build.cmd

--- build/scripts/build.cmd
+++ build/scripts/build.cmd
@@ -1,6 +1,6 @@
 set PATH=%PATH%;C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\ReadyRoll\OctoPack\tools
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat" -arch=amd64 -host_arch=amd64 -winsdk=10.0.16299.0 -app_platform=Desktop
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat" -test
+
 cd /D "%~dp0"
 cd ../../
 msbuild FabricObserver.sln /p:Configuration=Release /p:Platform=x64 /property:AppInsightsKey="c065641b-ec84-43fe-a8e7-c2bcbb697995"
