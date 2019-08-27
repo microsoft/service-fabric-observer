a101b8e17eabb127ac6bd595f0d90b7b7fb3a8ec
Charles Torre <torre_charles@hotmail.com>
2019-08-27 12:44:03 -0700

    Update test.cmd

--- build/scripts/test.cmd
+++ build/scripts/test.cmd
@@ -1,3 +1,4 @@
 cd /D "%~dp0"
+cd ../../
 
-vstest.console.exe ..\..\FabricObserverTests\bin\Release\FabricObserverTests.dll --blame /logger:trx
+vstest.console.exe FabricObserverTests\bin\Release\FabricObserverTests.dll --blame /logger:trx
