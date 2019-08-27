9ce2df19b485fd742b14b4fb6fbabd2549a707ad
Charles Torre <torre_charles@hotmail.com>
2019-08-27 14:42:41 -0700

    Update build.cmd

--- build/scripts/build.cmd
+++ build/scripts/build.cmd
@@ -1,17 +1,11 @@
 set PATH=%PATH%;C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\ReadyRoll\OctoPack\tools
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat" -arch=amd64 -host_arch=amd64 -winsdk=10.0.16299.0 -app_platform=Desktop
-
 cd /D "%~dp0"
 cd ../../
-
 nuget.exe restore "FabricObserver.sln"
 msbuild /t:Restore "FabricObserver.sln"
-
 msbuild /p:Configuration=Release /p:Platform=x64 /property:AppInsightsKey="c065641b-ec84-43fe-a8e7-c2bcbb697995" ".\TelemetryLib\TelemetryLib.csproj"
-
 msbuild /p:Configuration=Release /p:Platform=x64 ".\FabricObserver\FabricObserver.csproj"
-
-msbuild /p:Configuration=Release /p:Platform=AnyCPU ".\FabricObserverTests\FabricObserverTests.csproj"
-
+msbuild /p:Configuration=Release /p:Platform=x64 ".\FabricObserverTests\FabricObserverTests.csproj" /p:OutputPath="./"
 dotnet restore ".\FabricObserverWeb\FabricObserverWeb.csproj"
 dotnet build /p:Configuration=Release /p:Platform=AnyCPU ".\FabricObserverWeb\FabricObserverWeb.csproj"
