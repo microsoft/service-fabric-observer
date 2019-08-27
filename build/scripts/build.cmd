bb13df421af32c8468fbd75eebe9b8667df1dca0
Charles Torre <torre_charles@hotmail.com>
2019-08-27 13:52:28 -0700

    Update build.cmd

--- build/scripts/build.cmd
+++ build/scripts/build.cmd
@@ -3,11 +3,15 @@ call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Too
 
 cd /D "%~dp0"
 cd ../../
-nuget.exe restore "FabricObserver.sln"
-dotnet.exe restore "FabricObserver.sln"
 
+nuget.exe restore ".\TelemetryLib\TelemetryLib.csproj"
 msbuild /p:Configuration=Release /p:Platform=x64 /property:AppInsightsKey="c065641b-ec84-43fe-a8e7-c2bcbb697995" ".\TelemetryLib\TelemetryLib.csproj"
+
+nuget.exe restore ".\FabricObserver\FabricObserver.csproj"
 msbuild /p:Configuration=Release /p:Platform=x64 ".\FabricObserver\FabricObserver.csproj"
+
+nuget.exe restore ".\FabricObserverTests\FabricObserverTests.csproj"
 msbuild /p:Configuration=Release /p:Platform=AnyCPU ".\FabricObserverTests\FabricObserverTests.csproj"
+
 dotnet restore ".\FabricObserverWeb\FabricObserverWeb.csproj"
 msbuild /p:Configuration=Release /p:Platform=AnyCPU ".\FabricObserverWeb\FabricObserverWeb.csproj"
