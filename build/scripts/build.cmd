f2848f85a11e29c2bbc66d64acb700194a5af637
Charles Torre <torre_charles@hotmail.com>
2019-08-27 14:46:04 -0700

    Update build.cmd

--- build/scripts/build.cmd
+++ build/scripts/build.cmd
@@ -1,5 +1,5 @@
 set PATH=%PATH%;C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\ReadyRoll\OctoPack\tools
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat" -arch=amd64 -host_arch=amd64 -winsdk=10.0.16299.0 -app_platform=Desktop
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat"
 cd /D "%~dp0"
 cd ../../
 nuget.exe restore "FabricObserver.sln"
