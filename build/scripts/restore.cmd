a5b7d2c277fa36c869dd3e700a23da5578576dfc
Charles Torre <torre_charles@hotmail.com>
2019-08-27 12:20:56 -0700

    Update restore.cmd

--- build/scripts/restore.cmd
+++ build/scripts/restore.cmd
@@ -3,7 +3,8 @@ set PATH=%PATH%;C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\M
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat" -arch=amd64 -host_arch=amd64 -winsdk=10.0.16299.0 -app_platform=Desktop
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat" -test
 
-cd /D dp0"
+cd /D "%~dp0"
+msbuild ..\..\FabricObserver.sln
 
-nuget.exe restore "%~dp0\FabricObserver.sln"
-dotnet.exe restore "%~dp0\FabricObserver.sln"
+nuget.exe restore "..\..\FabricObserver.sln"
+dotnet.exe restore "..\..\FabricObserver.sln"
