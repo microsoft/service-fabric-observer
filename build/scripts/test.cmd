fa75e552b40aa4174b21c7b4d1d907a063f20224
Charles Torre <torre_charles@hotmail.com>
2019-08-28 12:59:29 -0700

    Delete test.cmd

--- build/scripts/test.cmd
+++
@@ -1,5 +0,0 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat" -test
-cd /D "%~dp0"
-cd ../../
-
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" "FabricObserverTests\bin\Release\FabricObserverTests.dll" --blame /logger:trx || exit /b 1
