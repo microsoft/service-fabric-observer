acf21aa67b138934f842855b30899f932304c888
Charles Torre <torre_charles@hotmail.com>
2019-08-27 18:17:29 -0700

    Update test.cmd

--- build/scripts/test.cmd
+++ build/scripts/test.cmd
@@ -2,4 +2,4 @@ call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Too
 cd /D "%~dp0"
 cd ../../
 
-vstest.console.exe FabricObserverTests\bin\Release\FabricObserverTests.dll --blame /logger:trx
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" "FabricObserverTests\bin\Release\FabricObserverTests.dll" --blame /logger:trx || exit /b 1
