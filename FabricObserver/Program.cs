82b6c9d5249fc2f3fb2744afee5367bc3e76b29d
Charles Torre <ctorre@microsoft.com>
2019-09-04 11:21:25 -0700

    StyleCop

--- FabricObserver/Program.cs
+++ FabricObserver/Program.cs
@@ -19,10 +19,6 @@ namespace FabricObserver
         {
             try
             {
-                // The ServiceManifest.XML file defines one or more service type names.
-                // Registering a service maps a service type name to a .NET type.
-                // When Service Fabric creates an instance of this service type,
-                // an instance of the class is created in this host process.
                 ServiceRuntime.RegisterServiceAsync(
                     "FabricObserverType",
                     context => new FabricObserver(context)).GetAwaiter().GetResult();
@@ -35,6 +31,7 @@ namespace FabricObserver
             catch (Exception e)
             {
                 ServiceEventSource.Current.ServiceHostInitializationFailed(e.ToString());
+
                 throw;
             }
         }
