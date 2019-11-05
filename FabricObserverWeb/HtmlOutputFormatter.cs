a837a97afc7ddd8be6bd74ba5dd0d74101875e91
Charles Torre <ctorre@microsoft.com>
2019-11-05 10:53:25 -0800

    ObserverManager ETW...

---
+++ FabricObserverWeb/HtmlOutputFormatter.cs
@@ -0,0 +1,21 @@
+﻿// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
+
+namespace FabricObserverWeb
+{
+    using Microsoft.AspNetCore.Mvc.Formatters;
+
+    // Added to support HTML output to callers...
+    public class HtmlOutputFormatter : StringOutputFormatter
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HtmlOutputFormatter"/> class.
+        /// </summary>
+        public HtmlOutputFormatter()
+        {
+            this.SupportedMediaTypes.Add("text/html");
+        }
+    }
+}
