32fcb8345f656813a22281e28fbe49412c9ea02a
Charles Torre <ctorre@microsoft.com>
2019-11-08 11:51:12 -0800

    License

--- TelemetryLib/GlobalSuppressions.cs
+++ TelemetryLib/GlobalSuppressions.cs
@@ -1,4 +1,9 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
+﻿// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
+
+// This file is used by Code Analysis to maintain SuppressMessage
 // attributes that are applied to this project.
 // Project-level suppressions either have no target or are given
 // a specific target and scoped to a namespace, type, member, etc.
