b220b09018c06c43de7a1372030c85ba306c2e84
Charles Torre <ctorre@microsoft.com>
2020-02-27 17:56:32 -0800

    stats

--- FabricObserver/Observers/Utilities/Statistics.cs
+++ FabricObserver/Observers/Utilities/Statistics.cs
@@ -9,93 +9,16 @@ using System.Linq;
 
 namespace FabricObserver.Utilities
 {
+    public enum WindowType
+    {
+        Max,
+        Min,
+    }
+
     public sealed class Statistics
     {
-        public static double StandardDeviation<T>(List<T> data)
-        {
-            var average = 0.0;
-            List<double> squaredMeanDifferences = new List<double>();
-
-            var v = data as List<long>;
-            if (v != null && v.Count > 0)
-            {
-                average = v.Average();
-                squaredMeanDifferences.AddRange(from n in v
-                                                select (n - average) * (n - average));
-            }
-
-            var x = data as List<int>;
-            if (x != null && x.Count > 0)
-            {
-                average = x.Average();
-                squaredMeanDifferences.AddRange(from n in x
-                                                select (n - average) * (n - average));
-            }
-
-            var y = data as List<float>;
-            if (y != null && y.Count > 0)
-            {
-                average = Convert.ToDouble(y.Average());
-                squaredMeanDifferences.AddRange(from n in y
-                                                select (n - average) * (n - average));
-            }
-
-            var z = data as List<double>;
-            if (z != null && z.Count > 0)
-            {
-                average = z.Average();
-                squaredMeanDifferences.AddRange(from n in z
-                                                select (n - average) * (n - average));
-            }
-
-            // Find the mean of those squared differences:
-            var meanOfSquaredDifferences = squaredMeanDifferences.Average();
-
-            // Standard Deviation is the square root of that mean:
-            var standardDeviation = Math.Sqrt(meanOfSquaredDifferences);
-
-            return standardDeviation;
-        }
-
-        public static double StandardDeviation2(IEnumerable<int> sequence)
-        {
-            if (sequence == null)
-            {
-                return -1;
-            }
-
-            double sum = 0;
-            double sumOfSquares = 0;
-            double count = 0;
-
-            foreach (var item in sequence)
-            {
-                count++;
-                sum += item;
-                sumOfSquares += item * item;
-            }
-
-            var variance = sumOfSquares - (sum * sum / count);
-
-            return Math.Sqrt(variance / count);
-        }
-
-        public static double StandardDeviation3(IEnumerable<double> sequence)
-        {
-            if (sequence == null)
-            {
-                return -1;
-            }
-
-            var computation = ComputeSumAndSumOfSquares(sequence);
-
-            var variance = computation.SumOfSquares - (computation.Sum * computation.Sum / computation.Count);
-
-            return Math.Sqrt(variance / computation.Count);
-        }
-
-        private static (int Count, double Sum, double SumOfSquares)
-            ComputeSumAndSumOfSquares(IEnumerable<double> sequence)
+        internal static (int Count, double Sum, double SumOfSquares)
+        ComputeSumAndSumOfSquares(IEnumerable<double> sequence)
         {
             double sum = 0;
             double sumOfSquares = 0;
@@ -111,16 +34,105 @@ namespace FabricObserver.Utilities
             return (count, sum, sumOfSquares);
         }
 
+        internal static double StandardDeviation<T>(List<T> data)
+        {
+            var average = 0.0;
+            var squaredMeanDifferences = new List<double>();
+
+            if (data is List<long> v && v.Count > 0)
+            {
+                average = v.Average();
+                squaredMeanDifferences.AddRange(from n in v
+                                                select (n - average) * (n - average));
+            }
+
+            if (data is List<int> x && x.Count > 0)
+            {
+                average = x.Average();
+                squaredMeanDifferences.AddRange(from n in x
+                                                select (n - average) * (n - average));
+            }
+
+            if (data is List<float> y && y.Count > 0)
+            {
+                average = Convert.ToDouble(y.Average());
+                squaredMeanDifferences.AddRange(from n in y
+                                                select (n - average) * (n - average));
+            }
+
+            if (data is List<double> z && z.Count > 0)
+            {
+                average = z.Average();
+                squaredMeanDifferences.AddRange(from n in z
+                                                select (n - average) * (n - average));
+            }
+
+            // Find the mean of those squared differences:
+            var meanOfSquaredDifferences = squaredMeanDifferences.Average();
+
+            // Standard Deviation is the square root of that mean:
+            var standardDeviation = Math.Sqrt(meanOfSquaredDifferences);
+
+            return standardDeviation;
+        }
+
         internal static double StandardDeviation(List<long> sequence)
         {
             var mean = sequence.Average();
+
             var squaredMeanDifferences = from n in sequence
                                          select (n - mean) * (n - mean);
+
             var meanOfSquaredDifferences = squaredMeanDifferences.Average();
 
             var standardDeviation = Math.Sqrt(meanOfSquaredDifferences);
 
             return standardDeviation;
+        }
+
+        internal List<int> SlidingWindow(
+                    List<int> data,
+                    int kwidth,
+                    WindowType windowType)
+        {
+            if (kwidth < 1 || data.Count == 0)
+            {
+                return null;
+            }
+
+            var map = new Dictionary<int, int>(data.Count);
+            var window = new List<int> { data.Count - kwidth + 1 };
+            var bst = new SortedSet<int>();
+
+            for (int i = 0; i < data.Count; i++)
+            {
+                var visit = data[i];
+                bst.Add(visit);
+                map[visit] = i;
+
+                if (i < kwidth - 1)
+                {
+                    continue;
+                }
+
+                if (i >= kwidth && map[data[i - kwidth]] == (i - kwidth))
+                {
+                    int k = data[i - kwidth];
+                    bst.Remove(k);
+                    map.Remove(k);
+                }
+
+                if (windowType == WindowType.Max)
+                {
+                    window.Insert(i - kwidth + 1, bst.Max);
+                }
+                else
+                {
+                    window.Insert(i - kwidth + 1, bst.Min);
+                }
+            }
+
+            return window;
         }
     }
 }
