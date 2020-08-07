6505c9ed98b9bc7ef284508198475efc4d6b2e5b
Charles Torre <ctorre@microsoft.com>
2020-08-07 10:38:24 -0700

    CO Build scripts

---
+++ Build-CONugetPackages.ps1
@@ -0,0 +1,32 @@
+function Build-Nuget {
+    param (
+        [string]
+        $packageId,
+
+        [string]
+        $basePath
+    )
+
+    [string] $nugetSpecTemplate = [System.IO.File]::ReadAllText([System.IO.Path]::Combine($scriptPath, "ClusterObserver.nuspec.template"))
+
+    [string] $nugetSpecPath = "$scriptPath\bin\release\ClusterObserver\$($packageId).nuspec"
+
+    [System.IO.File]::WriteAllText($nugetSpecPath,  $nugetSpecTemplate.Replace("%PACKAGE_ID%", $packageId))
+
+    .\nuget.exe pack $nugetSpecPath -basepath $basePath -OutputDirectory bin\release\ClusterObserver\Nugets -properties NoWarn=NU5100
+}
+
+[string] $scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+try {
+    Push-Location $scriptPath
+
+    Build-Nuget "ClusterObserver.Linux.SelfContained" "$scriptPath\bin\release\ClusterObserver\linux-x64\self-contained\ClusterObserverType"
+    Build-Nuget "ClusterObserver.Linux.FrameworkDependent" "$scriptPath\bin\release\ClusterObserver\linux-x64\framework-dependent\ClusterObserverType"
+
+    Build-Nuget "ClusterObserver.Windows.SelfContained" "$scriptPath\bin\release\ClusterObserver\win-x64\self-contained\ClusterObserverType"
+    Build-Nuget "ClusterObserver.Windows.FrameworkDependent" "$scriptPath\bin\release\ClusterObserver\win-x64\framework-dependent\ClusterObserverType"
+}
+finally {
+    Pop-Location
+}
