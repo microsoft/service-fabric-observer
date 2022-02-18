73bc11aa99d8d169c0a900d6920cfa7df74068e4
Christian Pappert <3459390+inputoutputcode@users.noreply.github.com>
2022-02-17 22:35:29 -0800

    first draft to deploy FO/CO as ARM application resource

---
+++ Documentation/Deployment/Deploy-FabricObserver.ps1
@@ -0,0 +1,17 @@
+﻿
+$subscriptionId = "" 
+Try {
+  Select-AzSubscription -SubscriptionId $subscriptionId -ErrorAction Stop
+} Catch {
+    Login-AzAccount
+    Set-AzContext -SubscriptionId $subscriptionId
+}
+
+$resourceGroup = "chrpap171850-group"
+$armTemplate = "service-fabric-observer.json"
+$armTemplateParameters = "service-fabric-observer.v3.1.20.parameters.json"
+
+cd "D:\Code\inputoutputcode\service-fabric-observer\Documentation\Deployment"
+
+New-AzResourceGroupDeployment -Name "deploy-service-fabric-observer" -ResourceGroupName $resourceGroup -TemplateFile $armTemplate -TemplateParameterFile $armTemplateParameters -Verbose -Mode Incremental
+
