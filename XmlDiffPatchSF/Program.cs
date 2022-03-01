3670271d6f7f71c64de668f3dadf954989db403c
Charles Torre <ctorre@microsoft.com>
2022-02-28 20:31:03 -0800

    Lic

--- XmlDiffPatchSF/Program.cs
+++ XmlDiffPatchSF/Program.cs
@@ -1,4 +1,8 @@
-﻿
+﻿// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
+
 using System;
 using System.Xml;
 using Microsoft.XmlDiffPatch;
