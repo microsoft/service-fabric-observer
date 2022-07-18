81be92fa9fbeaf56438956263a8951e4d7431a45
Charles Torre <ctorre@microsoft.com>
2022-07-18 14:57:19 -0700

    Lic

--- FabricObserver.Extensibility/Interfaces/ICpuUsage.cs
+++ FabricObserver.Extensibility/Interfaces/ICpuUsage.cs
@@ -3,10 +3,19 @@
 // Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
 // ------------------------------------------------------------
 
-namespace FabricObserver.Observers.Utilities
+namespace FabricObserver.Interfaces
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public interface ICpuUsage
     {
+        /// <summary>
+        /// Gets the current CPU usage for the supplied process identifier.
+        /// </summary>
+        /// <param name="procId">Process identifier.</param>
+        /// <param name="procName">Optional: Process name.</param>
+        /// <returns>Percentage of usage across all cores.</returns>
         double GetCurrentCpuUsagePercentage(int procId, string procName = null);
     }
 }
