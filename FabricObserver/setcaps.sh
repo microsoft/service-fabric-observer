02b39ed63455f07a852e7b189379ce726c198b64
Charles Torre <ctorre@microsoft.com>
2023-10-17 09:41:14 -0700

    Added net-tools install to setcaps.sh.

--- FabricObserver/setcaps.sh
+++ FabricObserver/setcaps.sh
@@ -2,3 +2,5 @@
 sudo setcap CAP_DAC_READ_SEARCH,CAP_SYS_PTRACE+p ./elevated_netstat
 sudo setcap CAP_DAC_READ_SEARCH,CAP_SYS_PTRACE+p ./elevated_proc_fd
 sudo setcap CAP_DAC_READ_SEARCH,CAP_DAC_OVERRIDE+p ./elevated_docker_stats
+
+sudo apt install net-tools
