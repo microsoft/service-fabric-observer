cbc64da26cc9e5d690af9310800ad55f1dcd07ac
Sidhant Bhatia <sibhatia@microsoft.com>
2025-06-19 12:29:36 -0400

    arm64 build changes

--- Build-ClusterObserver.ps1
+++ Build-ClusterObserver.ps1
@@ -1,28 +1,32 @@
+param(
+    [string] $RuntimeId = "win-x64",
+    [string] $Configuration = "Release"
+)
+
 $ErrorActionPreference = "Stop"
 
-$Configuration="Release"
 [string] $scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+[string] $winArmSFPackageRefOverride = "/p:VersionOverride_SFServices=7.0.1816"
 
+# For SF 11/12 arm64 builds, today we need to override the SF package reference version to match the current version of the SDK
+# to ensure arm64 x64 emulation works correctly
+if($RuntimeId -eq "win-arm64") {
+    $winArmSFPackageRefOverride = "/p:IsArmTarget=true"
+}
 try {
     Push-Location $scriptPath
 
     Remove-Item $scriptPath\bin\release\ClusterObserver\ -Recurse -Force -EA SilentlyContinue
 
-    dotnet publish ClusterObserver\ClusterObserver.csproj -o bin\release\ClusterObserver\linux-x64\self-contained\ClusterObserverType\ClusterObserverPkg\Code -c $Configuration -r linux-x64 --self-contained true
-    dotnet publish ClusterObserver\ClusterObserver.csproj -o bin\release\ClusterObserver\linux-x64\framework-dependent\ClusterObserverType\ClusterObserverPkg\Code -c $Configuration -r linux-x64 --self-contained false
-    dotnet publish ClusterObserver\ClusterObserver.csproj -o bin\release\ClusterObserver\win-x64\self-contained\ClusterObserverType\ClusterObserverPkg\Code -c $Configuration -r win-x64 --self-contained true
-    dotnet publish ClusterObserver\ClusterObserver.csproj -o bin\release\ClusterObserver\win-x64\framework-dependent\ClusterObserverType\ClusterObserverPkg\Code -c $Configuration -r win-x64 --self-contained false
+    dotnet publish ClusterObserver\ClusterObserver.csproj $winArmSFPackageRefOverride -o bin\release\ClusterObserver\$RuntimeId\self-contained\ClusterObserverType\ClusterObserverPkg\Code -c $Configuration -r $RuntimeId --self-contained true
+    dotnet publish ClusterObserver\ClusterObserver.csproj $winArmSFPackageRefOverride -o bin\release\ClusterObserver\$RuntimeId\framework-dependent\ClusterObserverType\ClusterObserverPkg\Code -c $Configuration -r $RuntimeId --self-contained false
+    
 
-    Copy-Item ClusterObserver\PackageRoot\* bin\release\ClusterObserver\linux-x64\self-contained\ClusterObserverType\ClusterObserverPkg\ -Recurse
-    Copy-Item ClusterObserver\PackageRoot\* bin\release\ClusterObserver\linux-x64\framework-dependent\ClusterObserverType\ClusterObserverPkg\ -Recurse
+    Copy-Item ClusterObserver\PackageRoot\* bin\release\ClusterObserver\$RuntimeId\self-contained\ClusterObserverType\ClusterObserverPkg\ -Recurse
+    Copy-Item ClusterObserver\PackageRoot\* bin\release\ClusterObserver\$RuntimeId\framework-dependent\ClusterObserverType\ClusterObserverPkg\ -Recurse
 
-    Copy-Item ClusterObserver\PackageRoot\* bin\release\ClusterObserver\win-x64\self-contained\ClusterObserverType\ClusterObserverPkg\ -Recurse
-    Copy-Item ClusterObserver\PackageRoot\* bin\release\ClusterObserver\win-x64\framework-dependent\ClusterObserverType\ClusterObserverPkg\ -Recurse
-
-    Copy-Item ClusterObserverApp\ApplicationPackageRoot\ApplicationManifest.xml bin\release\ClusterObserver\linux-x64\self-contained\ClusterObserverType\ApplicationManifest.xml
-    Copy-Item ClusterObserverApp\ApplicationPackageRoot\ApplicationManifest.xml bin\release\ClusterObserver\linux-x64\framework-dependent\ClusterObserverType\ApplicationManifest.xml
-    Copy-Item ClusterObserverApp\ApplicationPackageRoot\ApplicationManifest.xml bin\release\ClusterObserver\win-x64\self-contained\ClusterObserverType\ApplicationManifest.xml
-    Copy-Item ClusterObserverApp\ApplicationPackageRoot\ApplicationManifest.xml bin\release\ClusterObserver\win-x64\framework-dependent\ClusterObserverType\ApplicationManifest.xml
+    Copy-Item ClusterObserverApp\ApplicationPackageRoot\ApplicationManifest.xml bin\release\ClusterObserver\$RuntimeId\self-contained\ClusterObserverType\ApplicationManifest.xml
+    Copy-Item ClusterObserverApp\ApplicationPackageRoot\ApplicationManifest.xml bin\release\ClusterObserver\$RuntimeId\framework-dependent\ClusterObserverType\ApplicationManifest.xml
 }
 finally {
     Pop-Location
--- Build-FabricObserver.ps1
+++ Build-FabricObserver.ps1
@@ -1,7 +1,18 @@
+param(
+    [string] $RuntimeId = "win-x64",
+    [string] $Configuration = "Release"
+)
+
 $ErrorActionPreference = "Stop"
 
-$Configuration="Release"
 [string] $scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+[string] $winArmSFPackageRefOverride = "/p:VersionOverride_SFServices=7.0.1816"
+
+# For SF 11/12 arm64 builds, today we need to override the SF package reference version to match the current version of the SDK
+# to ensure arm64 x64 emulation works correctly
+if($RuntimeId -eq "win-arm64") {
+    $winArmSFPackageRefOverride = "/p:IsArmTarget=true"
+}
 
 try 
 {
@@ -9,32 +20,25 @@ try
 
     Remove-Item $scriptPath\bin\release\FabricObserver\ -Recurse -Force -EA SilentlyContinue
 
-    dotnet publish FabricObserver\FabricObserver.csproj -o bin\release\FabricObserver\linux-x64\self-contained\FabricObserverType\FabricObserverPkg\Code -c $Configuration -r linux-x64 --self-contained true
-    dotnet publish FabricObserver\FabricObserver.csproj -o bin\release\FabricObserver\linux-x64\framework-dependent\FabricObserverType\FabricObserverPkg\Code -c $Configuration -r linux-x64 --self-contained false
-    dotnet publish FabricObserver\FabricObserver.csproj -o bin\release\FabricObserver\win-x64\self-contained\FabricObserverType\FabricObserverPkg\Code -c $Configuration -r win-x64 --self-contained true
-    dotnet publish FabricObserver\FabricObserver.csproj -o bin\release\FabricObserver\win-x64\framework-dependent\FabricObserverType\FabricObserverPkg\Code -c $Configuration -r win-x64 --self-contained false
+    dotnet publish FabricObserver\FabricObserver.csproj $winArmSFPackageRefOverride -o bin\release\FabricObserver\$RuntimeId\self-contained\FabricObserverType\FabricObserverPkg\Code -c $Configuration -r $RuntimeId --self-contained true
+    dotnet publish FabricObserver\FabricObserver.csproj $winArmSFPackageRefOverride -o bin\release\FabricObserver\$RuntimeId\framework-dependent\FabricObserverType\FabricObserverPkg\Code -c $Configuration -r $RuntimeId --self-contained false
 
-    Copy-Item FabricObserver\PackageRoot\* bin\release\FabricObserver\linux-x64\self-contained\FabricObserverType\FabricObserverPkg\ -Recurse
-    Copy-Item FabricObserver\PackageRoot\* bin\release\FabricObserver\linux-x64\framework-dependent\FabricObserverType\FabricObserverPkg\ -Recurse
-
-    Copy-Item FabricObserver\PackageRoot\* bin\release\FabricObserver\win-x64\self-contained\FabricObserverType\FabricObserverPkg\ -Recurse
-    Copy-Item FabricObserver\PackageRoot\* bin\release\FabricObserver\win-x64\framework-dependent\FabricObserverType\FabricObserverPkg\ -Recurse
+    Copy-Item FabricObserver\PackageRoot\* bin\release\FabricObserver\$RuntimeId\self-contained\FabricObserverType\FabricObserverPkg\ -Recurse
+    Copy-Item FabricObserver\PackageRoot\* bin\release\FabricObserver\$RuntimeId\framework-dependent\FabricObserverType\FabricObserverPkg\ -Recurse
 
     # ApplicationManifest - All
-    Copy-Item FabricObserverApp\ApplicationPackageRoot\ApplicationManifest.xml bin\release\FabricObserver\linux-x64\self-contained\FabricObserverType\ApplicationManifest.xml
-    Copy-Item FabricObserverApp\ApplicationPackageRoot\ApplicationManifest.xml bin\release\FabricObserver\linux-x64\framework-dependent\FabricObserverType\ApplicationManifest.xml
-    Copy-Item FabricObserverApp\ApplicationPackageRoot\ApplicationManifest.xml bin\release\FabricObserver\win-x64\self-contained\FabricObserverType\ApplicationManifest.xml
-    Copy-Item FabricObserverApp\ApplicationPackageRoot\ApplicationManifest.xml bin\release\FabricObserver\win-x64\framework-dependent\FabricObserverType\ApplicationManifest.xml
+    Copy-Item FabricObserverApp\ApplicationPackageRoot\ApplicationManifest.xml bin\release\FabricObserver\$RuntimeId\self-contained\FabricObserverType\ApplicationManifest.xml
+    Copy-Item FabricObserverApp\ApplicationPackageRoot\ApplicationManifest.xml bin\release\FabricObserver\$RuntimeId\framework-dependent\FabricObserverType\ApplicationManifest.xml
 
     # ServiceManifest - Linux
-    Copy-Item FabricObserver\PackageRoot\ServiceManifest_linux.xml bin\release\FabricObserver\linux-x64\framework-dependent\FabricObserverType\FabricObserverPkg\ServiceManifest.xml -Force -Confirm:$False
-    Copy-Item FabricObserver\PackageRoot\ServiceManifest_linux.xml bin\release\FabricObserver\linux-x64\self-contained\FabricObserverType\FabricObserverPkg\ServiceManifest.xml -Force -Confirm:$False
+    Copy-Item FabricObserver\PackageRoot\ServiceManifest_linux.xml bin\release\FabricObserver\$RuntimeId\framework-dependent\FabricObserverType\FabricObserverPkg\ServiceManifest.xml -Force -Confirm:$False
+    Copy-Item FabricObserver\PackageRoot\ServiceManifest_linux.xml bin\release\FabricObserver\$RuntimeId\self-contained\FabricObserverType\FabricObserverPkg\ServiceManifest.xml -Force -Confirm:$False
 
-    # Get rid of ServiceManifest_linux.xml from build output.
-    Remove-Item bin\release\FabricObserver\linux-x64\framework-dependent\FabricObserverType\FabricObserverPkg\ServiceManifest_linux.xml -Force -Confirm:$False
-    Remove-Item bin\release\FabricObserver\win-x64\framework-dependent\FabricObserverType\FabricObserverPkg\ServiceManifest_linux.xml -Force -Confirm:$False
-    Remove-Item bin\release\FabricObserver\linux-x64\self-contained\FabricObserverType\FabricObserverPkg\ServiceManifest_linux.xml -Force -Confirm:$False
-    Remove-Item bin\release\FabricObserver\win-x64\self-contained\FabricObserverType\FabricObserverPkg\ServiceManifest_linux.xml -Force -Confirm:$False
+    if($RuntimeId -eq "linux-x64") {
+        # Get rid of ServiceManifest_linux.xml from build output.
+        Remove-Item bin\release\FabricObserver\$RuntimeId\framework-dependent\FabricObserverType\FabricObserverPkg\ServiceManifest_linux.xml -Force -Confirm:$False
+        Remove-Item bin\release\FabricObserver\$RuntimeId\self-contained\FabricObserverType\FabricObserverPkg\ServiceManifest_linux.xml -Force -Confirm:$False
+    }
 }
 finally 
 {
