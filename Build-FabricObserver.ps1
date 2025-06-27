7f24fd2f3b3394ba096641dbf142b1f10316b0ad
Sidhant Bhatia <sibhatia@microsoft.com>
2025-06-27 13:06:11 -0400

    automatic linux appmanifest element insertion code and added release notes

--- Build-FabricObserver.ps1
+++ Build-FabricObserver.ps1
@@ -47,6 +47,43 @@ try
         Copy-Item FabricObserver\PackageRoot\ServiceManifest_linux.xml bin\release\FabricObserver\$RuntimeId\framework-dependent\FabricObserverType\FabricObserverPkg\ServiceManifest.xml -Force -Confirm:$False
         Copy-Item FabricObserver\PackageRoot\ServiceManifest_linux.xml bin\release\FabricObserver\$RuntimeId\self-contained\FabricObserverType\FabricObserverPkg\ServiceManifest.xml -Force -Confirm:$False
 
+        # Modify ApplicationManifest.xml for Linux.Add commentMore actions
+    
+        # Load the XML file
+        [xml] $xml = Get-Content -Path "$scriptPath\bin\release\FabricObserver\linux-x64\self-contained\FabricObserverType\ApplicationManifest.xml"
+        
+        # Define the namespace manager
+        $namespaceManager = New-Object System.Xml.XmlNamespaceManager($xml.NameTable)
+        $namespaceManager.AddNamespace("fab", "http://schemas.microsoft.com/2011/01/fabric")
+
+        # Find the Policies node using the namespace
+        $policiesNode = $xml.SelectSingleNode("//fab:Policies", $namespaceManager)
+
+        if ($policiesNode -ne $null) 
+        {
+            # Create the new RunAsPolicy element
+            $runAsPolicy = $xml.CreateElement("RunAsPolicy", $xml.DocumentElement.NamespaceURI)
+            $runAsPolicy.SetAttribute("CodePackageRef", "Code")
+            $runAsPolicy.SetAttribute("UserRef", "SystemUser")
+            $runAsPolicy.SetAttribute("EntryPointType", "Setup")
+            
+            # Import the node into the document to avoid xmlns duplication
+            $importedNode = $xml.ImportNode($runAsPolicy, $true)
+
+            # Append the new element
+            $policiesNode.AppendChild($importedNode) | Out-Null
+
+            # Save the updated AppManifest to both self-contained and framework-dependent directories.   
+            $xml.Save("$scriptPath\bin\release\FabricObserver\linux-x64\framework-dependent\FabricObserverType\ApplicationManifest.xml")
+            $xml.Save("$scriptPath\bin\release\FabricObserver\linux-x64\self-contained\FabricObserverType\ApplicationManifest.xml")
+
+            Write-Host "RunAsPolicy node inserted successfully."
+        } 
+        else 
+        {
+            Write-Host "Policies node not found."
+        }
+
         # Get rid of ServiceManifest_linux.xml from build output.
         Remove-Item bin\release\FabricObserver\$RuntimeId\framework-dependent\FabricObserverType\FabricObserverPkg\ServiceManifest_linux.xml -Force -Confirm:$False
         Remove-Item bin\release\FabricObserver\$RuntimeId\self-contained\FabricObserverType\FabricObserverPkg\ServiceManifest_linux.xml -Force -Confirm:$False
